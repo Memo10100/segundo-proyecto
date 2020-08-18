528275bdc178461519a5c8166240c153a19275a1
Memo10100 <juaniiiii1025@gmail.com>
2020-08-18 18:26:42 -0300

    primer commit

---
+++ segundo proyecto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace segundo_proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dado = new Random();
+            int aleatorio1 = dado.Next(1, 11);
+            int aleatorio2 = dado.Next(11, 21);
+            Console.WriteLine("El numero del 1º dado es {0}", aleatorio1);
+            Console.WriteLine("El numero del 2º dado es {0}", aleatorio2);
+            Console.ReadKey();
+        }
+}
+}
